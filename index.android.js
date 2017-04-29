a7da8e6a6241ad54e1044fcfb871ae3ca501a9e5
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2017-04-29 20:19:40 +0100

    Animate adding items to FlatList

---
+++ index.android.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import AnimatedVirtualizedList from './AnimatedVirtualizedList';
+
+AppRegistry.registerComponent('AnimatedVirtualizedList', () => AnimatedVirtualizedList);
